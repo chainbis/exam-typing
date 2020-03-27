191df9ef951d340ad8e9ab26478bbc81a57ffc67
Chain S <chainbis@gmail.com>
2020-03-28 01:09:50 +0700

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'typing';
+export class AppComponent implements OnInit {
+ randText = '';
+ typing = '';
+ foundError = false;
+  ngOnInit() {
+    this.randText = lorem.words();
+  }
+  onChange(inputText: string) {
+    this.typing = inputText;
+  }
+  charClass(char: string, i: number): string {
+    if (i >= this.typing.length){
+      return '';
+    }
+    if (this.typing[i] === char) {
+      if (i === this.typing.length - 1 ) {
+
+      }
+      return 'green';
+    } else {
+      this.foundError  = false;
+      return 'red';
+    }
+  }
 }
